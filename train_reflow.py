1eca90f6ef75d999f1ad0396af2bcae53a0f2063
Ka Hei Hui <khui@nvidia.com>
2024-07-03 12:40:04 +0800

    1. change reflow

--- train_reflow.py
+++ train_reflow.py
@@ -483,7 +483,7 @@ def main():
 def parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataroot', default='./data/ShapeNetCore.v2.PC15k/')
+    parser.add_argument('--dataroot', default='../ShapeNetCore.v2.PC15k/')
     parser.add_argument('--category', default='chair')
 
     parser.add_argument('--bs', type=int, default=96, help='input batch size')
