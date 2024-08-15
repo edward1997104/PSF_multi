3d82d74e242ca1bd8b92879ca1cea1c944cca6c7
Ka Hei Hui <khui@nvidia.com>
2024-08-15 15:33:56 +0800

    1. update inference script

---
+++ submit_test.sh
@@ -0,0 +1,7 @@
+python test_flow.py --category $1 --model $2 --step 1
+python test_flow.py --category $1 --model $2 --step 2
+python test_flow.py --category $1 --model $2 --step 5
+python test_flow.py --category $1 --model $2 --step 10
+python test_flow.py --category $1 --model $2 --step 20
+python test_flow.py --category $1 --model $2 --step 50
+python test_flow.py --category $1 --model $2 --step 100
